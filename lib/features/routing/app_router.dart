0ac1f115a094f36de0ac772d77d31b97d65410aa
Armando Quintana <aquintana@8thlight.com>
2025-02-12 15:32:07 -0600

    Adds initialLocation + signUp route

--- lib/features/routing/app_router.dart
+++ lib/features/routing/app_router.dart
@@ -3,22 +3,32 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_starter_kit/common_widgets/common_scaffold.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import '../counter/presentation/counter_screen.dart';
+import '../../features/authentication/presentation/login_screen/login_screen.dart';
+import '../../features/authentication/presentation/sign_up_screen/sign_up_screen.dart';
 
 part 'app_router.g.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
 
-enum AppRoute { counter }
+enum AppRoute { counter, login, signUp }
 
 @Riverpod(keepAlive: true)
 GoRouter goRouter(Ref ref) {
-  return GoRouter(navigatorKey: _rootNavigatorKey, routes: [
-    GoRoute(
-      path: '/',
-      name: AppRoute.counter.name,
-      pageBuilder: (context, state) =>
-          const NoTransitionPage(child: CommonScaffold(body: CounterScreen())),
-    ),
-  ]);
+  return GoRouter(
+      initialLocation: '/login',
+      navigatorKey: _rootNavigatorKey,
+      routes: [
+        GoRoute(
+          path: '/login',
+          name: AppRoute.login.name,
+          pageBuilder: (context, state) => const NoTransitionPage(
+              child: CommonScaffold(body: LoginScreen())),
+        ),
+        GoRoute(
+          path: '/signUp',
+          name: AppRoute.signUp.name,
+          pageBuilder: (context, state) => const NoTransitionPage(
+              child: CommonScaffold(body: SignUpScreen())),
+        ),
+      ]);
 }

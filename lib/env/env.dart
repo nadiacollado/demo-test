d328d9751c175ffa6e3a3932a055d17015bfdcdf
Nadia Collado <ncollado@8thlight.com>
2025-02-12 13:49:42 -0500

    Add ios/android env variables

---
+++ lib/env/env.dart
@@ -0,0 +1,31 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+final class Env {
+  @EnviedField(varName: 'FIREBASE_MESSAGING_SENDER_ID', obfuscate: true)
+  static final String firebaseMessagingSenderId =
+      _Env.firebaseMessagingSenderId;
+
+  @EnviedField(varName: 'FIREBASE_PROJECT_ID', obfuscate: true)
+  static final String firebaseProjectId = _Env.firebaseProjectId;
+
+  @EnviedField(varName: 'FIREBASE_STORAGE_BUCKET', obfuscate: true)
+  static final String firebaseStorageBucket = _Env.firebaseStorageBucket;
+
+  @EnviedField(varName: 'ANDROID_API_KEY', obfuscate: true)
+  static final String androidApiKey = _Env.androidApiKey;
+
+  @EnviedField(varName: 'ANDROID_APP_ID', obfuscate: true)
+  static final String androidAppId = _Env.androidAppId;
+
+  @EnviedField(varName: 'IOS_API_KEY', obfuscate: true)
+  static final String iosApiKey = _Env.iosApiKey;
+
+  @EnviedField(varName: 'IOS_APP_ID', obfuscate: true)
+  static final String iosAppId = _Env.iosAppId;
+
+  @EnviedField(varName: 'IOS_BUNDLE_ID', obfuscate: true)
+  static final String iosBundleId = _Env.iosBundleId;
+}

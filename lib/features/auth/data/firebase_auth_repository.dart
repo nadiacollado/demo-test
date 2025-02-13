8c91e1a0dd4e260d6bf72138cfe0429310b8bb3a
Nadia Collado <ncollado@8thlight.com>
2025-02-13 12:15:40 -0500

    Add auth repository

---
+++ lib/features/auth/data/firebase_auth_repository.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+part 'firebase_auth_repository.g.dart';
+
+class AuthRepository {
+  AuthRepository(this._auth);
+  final FirebaseAuth _auth;
+
+  Stream<User?> authStateChanges() => _auth.authStateChanges();
+  User? get currentUser => _auth.currentUser;
+
+  Future<UserCredential> signInWithEmailPassword(
+      String email, String password) async {
+    return await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<UserCredential> signUpWithEmailPassword(
+      String email, String password) async {
+    return await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
+
+@Riverpod(keepAlive: true)
+FirebaseAuth firebaseAuth(Ref ref) {
+  return FirebaseAuth.instance;
+}
+
+@Riverpod(keepAlive: true)
+AuthRepository authRepository(Ref ref) {
+  return AuthRepository(ref.watch(firebaseAuthProvider));
+}
+
+@riverpod
+Stream<User?> authStateChanges(Ref ref) {
+  return ref.watch(authRepositoryProvider).authStateChanges();
+}

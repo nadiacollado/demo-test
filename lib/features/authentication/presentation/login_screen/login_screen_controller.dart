a1e71217c26a6a821ebd13ff0dcfd6b8e76149ff
Nadia Collado <ncollado@8thlight.com>
2025-02-13 15:22:43 -0500

    Add login controller

---
+++ lib/features/authentication/presentation/login_screen/login_screen_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_starter_kit/features/authentication/data/firebase_auth_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'login_screen_controller.g.dart';
+
+@riverpod
+class LoginScreenController extends _$LoginScreenController {
+  @override
+  FutureOr<void> build() {
+    // no op
+  }
+
+    Future<void> signInWithEmailPassword(String email, String password) async {
+    final authRepository = ref.read(authRepositoryProvider);
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(
+        () => authRepository.signInWithEmailPassword(email, password));
+  }
+}
---
+++ lib/features/authentication/presentation/login_screen/login_screen_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_screen_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginScreenControllerHash() =>
+    r'5c1c07156c049df8fb983abd7269631af6ad8fa0';
+
+/// See also [LoginScreenController].
+@ProviderFor(LoginScreenController)
+final loginScreenControllerProvider =
+    AutoDisposeAsyncNotifierProvider<LoginScreenController, void>.internal(
+  LoginScreenController.new,
+  name: r'loginScreenControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$loginScreenControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LoginScreenController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package

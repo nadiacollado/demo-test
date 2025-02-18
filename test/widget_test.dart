2af6498f6b59dc8861c1b568bb9da6595a3fb255
Serene Aronow <JennSerene@gmail.com>
2025-02-18 16:42:38 -0500

    Fixed test for CI verification

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_starter_kit/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_starter_kit/features/counter/presentation/counter_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -14,20 +15,25 @@ void main() {
     'Counter increments smoke test',
     (WidgetTester tester) async {
       // Build our app and trigger a frame.
-      await tester.pumpWidget(const MyApp());
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: ProviderScope(
+            child: CounterScreen(),
+          ),
+        ),
+      );
 
       // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
+      expect(find.text('Counter: 0'), findsOneWidget);
+      expect(find.text('Counter: 1'), findsNothing);
 
       // Tap the '+' icon and trigger a frame.
       await tester.tap(find.byIcon(Icons.add));
       await tester.pump();
 
       // Verify that our counter has incremented.
-      expect(find.text('0'), findsNothing);
-      expect(find.text('1'), findsOneWidget);
+      expect(find.text('Counter: 0'), findsNothing);
+      expect(find.text('Counter: 1'), findsOneWidget);
     },
-    skip: true,
   );
 }
